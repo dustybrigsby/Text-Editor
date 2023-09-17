7503e6a89575e4442f0336a5945e983ad8f592b2
Dusty Brigsby <dustybrigsby@gmail.com>
2023-09-17 09:50:21 -0700

    enabled HMR

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -17,13 +17,15 @@ module.exports = () => {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
+    devServer: {
+      hot: 'only',
+    },
     plugins: [
       new HtmlWebpackPlugin({
         template: './index.html',
         title: 'Webpack Plugin'
-      })
+      }),
     ],
-
     module: {
       rules: [
         {
